99ff107b516e08fa0cffb4c07e6a66d2358515da
Shaun <scpour@gmail.com>
2022-10-03 14:37:56 -0400

    More Projects

--- js/app.js
+++ js/app.js
@@ -3,10 +3,13 @@ const closeMenu = document.querySelector('.closeMenu');
 const openMenu = document.querySelector('.openMenu');
 const open = document.getElementById('openAPIProj');
 const openGameShow = document.getElementById('openGameShow');
+const openMarkdownPreviewer = document.getElementById('openMarkdownPreviewer');
 const modalContainer = document.getElementById('modal_container_reactapi');
 const gameShowContainer = document.getElementById('modal_container_game_show');
+const markdownContainer = document.getElementById('modal_container_reactmarkdown');
 const close = document.getElementById('closeAPIProj');
 const closeGameShow = document.getElementById('closeGameShowProj');
+const closeMarkdownProj = document.getElementById('closeMarkdownProj');
 const menuItm = document.querySelectorAll('.mainMenu li a');
 
 openMenu.addEventListener('click', () => {
@@ -28,10 +31,19 @@ close.addEventListener('click', () => {
 openGameShow.addEventListener('click', () => {
     gameShowContainer.classList.add('show');
 });
-
 closeGameShow.addEventListener('click', () => {
     gameShowContainer.classList.remove('show');
 });
+
+openMarkdownPreviewer.addEventListener('click', () => {
+    markdownContainer.classList.add('show');
+});
+
+closeMarkdownProj.addEventListener('click', () => {
+    markdownContainer.classList.remove('show');
+})
+
+
 
 menuItm.forEach((menuClose => {
     menuClose.addEventListener('click', () => {
