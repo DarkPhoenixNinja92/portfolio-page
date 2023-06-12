c6fe43cba1d03b36784f1a5937158d2e7cca8e68
Shaun <scpour@gmail.com>
2023-06-12 13:06:49 -0400

    Set up mobile menu to  allow the menu to be opened and closed and for the menu drawer to take up the entire height of the page when opened

---
+++ js/script.js
@@ -0,0 +1,15 @@
+const mobileMenu = document.querySelector('.nav-list');
+const mobileMenuOpen = document.querySelector('.mobile-menu');
+const mobileMenuClose = document.querySelector('.mobile-menu-close');
+
+mobileMenuOpen.addEventListener('click', () => {
+    mobileMenu.classList.add('nav-show');
+    mobileMenuOpen.style.display = 'none';
+    mobileMenuClose.style.display = 'initial';
+});
+
+mobileMenuClose.addEventListener('click', () => {
+    mobileMenu.classList.remove('nav-show');
+    mobileMenuOpen.style.display = 'initial';
+    mobileMenuClose.style.display = 'none';
+})
