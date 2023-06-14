5e60331ac0c4aa97c73f149246de6613ecdf6e57
Shaun <scpour@gmail.com>
2023-06-14 10:33:22 -0400

    Modified skill section to include fontawesome icons representing my skills as well as how long I've been using them for.

--- js/script.js
+++ js/script.js
@@ -3,13 +3,17 @@ const mobileMenuOpen = document.querySelector('.mobile-menu');
 const mobileMenuClose = document.querySelector('.mobile-menu-close');
 
 mobileMenuOpen.addEventListener('click', () => {
-    mobileMenu.classList.add('nav-show');
+    mobileMenu
+        .classList
+        .add('nav-show');
     mobileMenuOpen.style.display = 'none';
     mobileMenuClose.style.display = 'initial';
 });
 
 mobileMenuClose.addEventListener('click', () => {
-    mobileMenu.classList.remove('nav-show');
+    mobileMenu
+        .classList
+        .remove('nav-show');
     mobileMenuOpen.style.display = 'initial';
     mobileMenuClose.style.display = 'none';
 })
