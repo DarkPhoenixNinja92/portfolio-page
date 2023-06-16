8481a3bd7b7b61a6e70fbb16e0330e5036d427c1
Shaun <scpour@gmail.com>
2023-06-16 12:05:48 -0400

    Modified javascript code to allow the user to view a specific group of projects based on what button they press.

--- js/script.js
+++ js/script.js
@@ -1,8 +1,14 @@
 const mobileMenu = document.querySelector('.nav-list');
 const mobileMenuOpen = document.querySelector('.mobile-menu');
 const mobileMenuClose = document.querySelector('.mobile-menu-close');
-const main = document.querySelector('main');
+let main = document.querySelector('main');
 const body = document.querySelector('body');
+const allBtn = document.querySelector('.all');
+const personalBtn = document.querySelector('.personal');
+const educationBtn = document.querySelector('.educational');
+const personal = document.querySelector('.personal-projects');
+const educational = document.querySelector('.educational-projects');
+let visibleProjects = 'all';
 
 mobileMenuOpen.addEventListener('click', () => {
     main.style.position = 'absolute';
@@ -23,4 +29,27 @@ mobileMenuClose.addEventListener('click', () => {
         .remove('nav-show');
     mobileMenuOpen.style.display = 'initial';
     mobileMenuClose.style.display = 'none';
+});
+
+allBtn.addEventListener('click', () => {
+    if(visibleProjects != 'all') {
+        visibleProjects = 'all';
+        personal.style.display = 'initial';
+        educational.style.display = 'initial';
+    }
+});
+personalBtn.addEventListener('click', () => {
+    if(visibleProjects != 'personal') {
+        visibleProjects = 'personal';
+        personal.style.display = 'initial';
+        educational.style.display = 'none';
+    }
+});
+
+educationBtn.addEventListener('click', () => {
+    if(visibleProjects != 'educational') {
+        visibleProjects = 'educational';
+        educational.style.display = 'initial';
+        personal.style.display = 'none';
+    }
 })
